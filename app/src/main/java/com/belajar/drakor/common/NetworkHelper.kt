62041439939a084029dadab2fe3c159b06f675d0
Sopianna Siagian <ssopianna@gmail.com>
2024-07-06 21:51:10 +0700

    implement chucker

--- app/src/main/java/com/belajar/drakor/common/NetworkHelper.kt
+++ app/src/main/java/com/belajar/drakor/common/NetworkHelper.kt
@@ -1,6 +1,9 @@
 package com.belajar.drakor.common
 
+import android.content.Context
 import com.belajar.drakor.data.datasource.remote.TMDB_TOKEN
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.google.gson.Gson
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
@@ -9,14 +12,15 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkHelper {
-    fun createRetrofit(baseUrl: String): Retrofit {
+    fun createRetrofit(baseUrl: String, context: Context): Retrofit {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create(Gson()))
+            .client(createOkHttpClient(context))
             .build()
     }
 
-    fun createOkHttpClient(): OkHttpClient {
+    fun createOkHttpClient(context: Context): OkHttpClient {
         val loggingInterceptor = HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         }
@@ -29,9 +33,18 @@ object NetworkHelper {
             chain.proceed(request)
         }
 
+        val chuckerInterceptor = ChuckerInterceptor.Builder(context)
+            .collector(ChuckerCollector(context))
+            .maxContentLength(250_000L)
+            .redactHeaders(emptySet())
+            .alwaysReadResponseBody(true)
+            .build()
+
         return OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .addInterceptor(authInterceptor)
+            .addInterceptor(chuckerInterceptor)
             .build()
     }
 }
+
--- app/src/main/java/com/belajar/drakor/data/di/DataModule.kt
+++ app/src/main/java/com/belajar/drakor/data/di/DataModule.kt
@@ -1,11 +1,15 @@
 package com.belajar.drakor.data.di
 
+import android.content.Context
 import com.belajar.drakor.common.NetworkHelper
 import com.belajar.drakor.data.datasource.remote.ApiService
 import com.belajar.drakor.data.datasource.remote.PeopleRemoteDataSource
 import com.belajar.drakor.data.datasource.remote.TMDB_TOKEN
 import com.belajar.drakor.data.repository.PeopleRepositoryImpl
 import com.belajar.drakor.domain.repository.PeopleRepository
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.google.gson.Gson
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -14,20 +18,20 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val dataModule = module {
-    single { NetworkHelper.createRetrofit("https://api.themoviedb.org/3/").create(ApiService::class.java) }
+    single { (context: Context) -> NetworkHelper.createRetrofit("https://api.themoviedb.org/3/", context).create(ApiService::class.java) }
     single { PeopleRemoteDataSource(get()) }
     single<PeopleRepository> { PeopleRepositoryImpl(get()) }
 }
 
-private fun provideRetrofit(): Retrofit {
+private fun provideRetrofit(context: Context): Retrofit {
     return Retrofit.Builder()
         .baseUrl("https://api.themoviedb.org/3/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(provideOkHttpClient())
+        .addConverterFactory(GsonConverterFactory.create(Gson()))
+        .client(provideOkHttpClient(context))
         .build()
 }
 
-private fun provideOkHttpClient(): OkHttpClient {
+private fun provideOkHttpClient(context: Context): OkHttpClient {
     val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
@@ -40,9 +44,18 @@ private fun provideOkHttpClient(): OkHttpClient {
         chain.proceed(request)
     }
 
+    val chuckerInterceptor = ChuckerInterceptor.Builder(context)
+        .collector(ChuckerCollector(context))
+        .maxContentLength(250_000L)
+        .redactHeaders(emptySet())
+        .alwaysReadResponseBody(true)
+        .build()
+
     return OkHttpClient.Builder()
         .addInterceptor(loggingInterceptor)
         .addInterceptor(authInterceptor)
+        .addInterceptor(chuckerInterceptor)
         .build()
 }
+
 
